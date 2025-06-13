2160db318b79927fdad41a4f030a76e74e6b1e48
normalcat <tntgav@gmail.com>
2025-06-13 14:56:41 -0700

    make inertia deck even harsher

--- Items/decks.lua
+++ Items/decks.lua
@@ -12,8 +12,8 @@ SMODS.Back {
     apply = function()
         config_reset()
         G.GAME.disable_ante_gain = true
-        G.GAME.ante_config.base_exponent = 1.5
-        G.GAME.ante_config.arrow_exponent = 1.1
+        G.GAME.ante_config.base_exponent = 1.2
+        G.GAME.ante_config.arrow_exponent = 1.05
         G.GAME.ante_config.ante_exponent = 2
     end
 }
--- debug.lua
+++ debug.lua
@@ -1,16 +1,6 @@
-local v = #G.GAME.tags
-for i=1,v do
+local n = to_big(100):arrow(50,100)
+n = n:arrow(100,n)
 
-    local key = G.GAME.tags[1].key
-    if key == "tag_entr_ascendant_cat" or key == "tag_cry_cat" or key == "tag_entr_ascendant_dog" or key == "tag_entr_dog" then
-        G.GAME.tags[1]:remove()
-    end
-
-    if G.GAME.tags[i] then
-        key = G.GAME.tags[i].key
-        if key == "tag_entr_ascendant_cat" or key == "tag_cry_cat" or key == "tag_entr_ascendant_dog" or key == "tag_entr_dog" then
-            G.GAME.tags[i]:remove()
-        end
-    end 
-
+for i=(1e6-5),(1e6+10) do
+    print(tostring(i) .."-ation: " .. tostring(n:arrow(i,n)))
 end
