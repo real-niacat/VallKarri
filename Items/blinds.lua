967afc1caf01db7a287d9fb130ed5aa2b1251e87
normalcat <tntgav@gmail.com>
2025-07-02 10:36:40 -0700

    fix fanning flames for freeeeee

--- Items/blinds.lua
+++ Items/blinds.lua
@@ -3,8 +3,9 @@ SMODS.Blind {
     loc_txt = {
         name = "Fanning Flames",
         text = {
-            "All played cards become rental and marked for death",
+            "All played cards become marked for death",
             "Marked for death has no visual indicator",
+            "Cards held in hand become rental",
             "Must play at least 4 cards"
         }
     },
@@ -13,9 +14,17 @@ SMODS.Blind {
     dollars = 0,
     mult = 1e100,
     calculate = function(self, blind, context)
-		if context.individual and context.cardarea == G.play and context.other_card then
-			context.other_card.ability.valk_marked_for_death = true
+		if context.after then
+            for i,c in ipairs(G.play.cards) do
+			    c.ability.valk_marked_for_death = true
+            end
 		end
+
+        if context.before then
+            for i,c in ipairs(G.hand.cards) do
+                c.ability.rental = true
+            end
+        end
 	end,
     debuff_hand = function(self, cards, hand, handname, check)
         return (#cards < 4)
