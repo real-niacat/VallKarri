d18f4a5621616cb671048fcd6ed0ae22e24abac8
normalcat <tntgav@gmail.com>
2025-08-07 08:17:52 -0700

    infoqueue

--- Items/Jokers/exotic.lua
+++ Items/Jokers/exotic.lua
@@ -361,6 +361,7 @@ SMODS.Joker {
     },
     config = { extra = { tags = 2, tags_base = 2, inc = 1, max = 10, mult = 5 } },
     loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue+1] = G.P_TAGS.tag_meteor
         return {vars = { card.ability.extra.tags, card.ability.extra.inc, card.ability.extra.max, card.ability.extra.tags_base, card.ability.extra.mult } }
     end,
     rarity = "cry_exotic",
