b08566e18a16935e83194e50b283d7e496084c0f
normalcat <tntgav@gmail.com>
2025-08-19 23:06:55 -0700

    new edition & fixes to existing editions

--- Items/editions.lua
+++ Items/editions.lua
@@ -18,14 +18,20 @@ SMODS.Edition {
             shadercredit("Lily Felli")
         }
     },
-    config = {extra = {echips = 1.05}},
+    config = {echips = 1.05},
     loc_vars = function(self, info_queue, card)
         return {vars = {card and card.edition and card.edition.echips or self.config.echips}}
     end,
     weight = 1,
     get_weight = function(self)
         return G.GAME.edition_rate * self.weight
-    end
+    end,
+    calculate = function(self, card, context)
+        if (context.edition and context.cardarea == G.jokers and card.config.trigger ) 
+        or (context.main_scoring and context.cardarea == G.play) then
+            SMODS.calculate_effect({echips = card.edition.echips}, card)
+        end
+    end,
 }
 
 SMODS.Shader {
@@ -58,4 +64,41 @@ SMODS.Edition {
     get_weight = function(self)
         return G.GAME.edition_rate * self.weight
     end
+}
+
+SMODS.Shader {
+    key = "rgb",
+    path = "rgb.fs"
+}
+
+SMODS.Edition {
+    key = "rgb",
+    shader = "rgb",
+    loc_txt = {
+        name = "R.G.B.",
+        label = "R.G.B.",
+        text = {
+            "{C:mult}+#1#{} Mult and {X:mult,C:white}X#2#{} Mult",
+            "{C:chips}+#1#{} Chips and {X:chips,C:white}X#2#{} Chips",
+            shadercredit("Lily Felli")
+        }
+    },
+    config = {mult = 5, xmult = 1.2, chips = 5, xchips = 1.2},
+    loc_vars = function(self, info_queue, card)
+        -- print(self)
+        return {vars = { 
+            card and card.edition and card.edition.mult or self.config.mult,
+            card and card.edition and card.edition.xmult or self.config.xmult
+         }}
+    end,
+    weight = 4,
+    get_weight = function(self)
+        return G.GAME.edition_rate * self.weight
+    end,
+    calculate = function(self, card, context)
+        if (context.edition and context.cardarea == G.jokers and card.config.trigger ) 
+        or (context.main_scoring and context.cardarea == G.play) then
+            SMODS.calculate_effect({mult = card.edition.mult, chips = card.edition.mult, xmult = card.edition.xmult, xchips = card.edition.xchips}, card)
+        end
+    end,
 }
