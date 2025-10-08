a0a94013b2c8b5a6ac10b2b536fa489328c273a1
normalcat <tntgav@gmail.com>
2025-10-07 17:14:07 -0700

    patchy oc quips

--- Items/quips.lua
+++ Items/quips.lua
@@ -15,6 +15,19 @@ local quips = {
         ["j_valk_illena"] = {
             { "the magic illena-ball says:", "you lose! maybe next time?" },
             { "it's okay, you",              "tried your best" }
+        },
+        ["j_valk_kathleen"] = {
+            { "A miscalculation,", "I'm sure...", "..Right?" },
+            { "Next time...", "Next time you'll", "beat it for sure!" },
+            { "You've got this.", "Take a break or do", "something else." },
+            { "That looked difficult.", "Tough luck." },
+        },
+        ["j_valk_sinep"] = {
+            { "i need a drink." },
+            { "*modifies your joker values*", "what do you MEAN i'm", "not allowed to do that now??" },
+            { "lMAO oWNED!!!11!!" },
+            { "*snap*", "this is going", "in my cringe compilation" },
+            { "do that again and", "i'll reduce you to a shape"},
         }
     },
     win = {
@@ -30,8 +43,18 @@ local quips = {
         ["j_valk_illena"] = {
             { "the magic illena-ball says:", "you win! congrats!" },
             { "smile, i'll hang",            "it up on my wall!" }
+        },
+        ["j_valk_kathleen"] = {
+            { "Oh sweet!",     "Got the balls to", "go further?" },
+            { "Nice tactics!", "Can I have a go?" },
+            { "meow meow",     "meow meow" },
+        },
+        ["j_valk_sinep"] = {
+            { "TAke a look,",      "yall:",              "IMG_5491.jpg" },
+            { "i need a drink." },
+            { "if you go further", "i'll DO something.", "not saying what, though" },
+            { ":rabbit:" },
         }
-
     }
 
 }
