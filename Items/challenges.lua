be129928f1931f91fdf08df990d38aa6e5a93c01
normalcat <tntgav@gmail.com>
2025-10-18 15:49:39 -0700

    this check is ugly

--- Items/challenges.lua
+++ Items/challenges.lua
@@ -37,7 +37,7 @@ function ease_dollars(amount, instant)
     if amount ~= to_big(0) then
         amount = (math.abs(amount) ^ (G.GAME.money_exponent or 1)) * (amount / math.abs(amount))
     end
-    moneyhook(amount, instant)
+    moneyhook(amount == amount and amount or 0, instant)
 end
 
 local emplace = CardArea.emplace
